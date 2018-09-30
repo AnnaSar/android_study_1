524ef21bd863b48c4aea16f7e2f64aa58e783831
Ancha.lynx <ancha.lynx@gmail.com>
2018-10-01 00:18:59 +0700

    commit f_new_layout_les_1

---
+++ app/src/main/java/ActivityAbout.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lynx.ancha.lesson_1.R;
+
+public class ActivityAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        //BuildConfig.APPLICATION_ID
+    }
+}
